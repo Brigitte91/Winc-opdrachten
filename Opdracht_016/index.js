1d278c0237344cd3b9883d9a00b45b1f30d461ce
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2021-01-28 00:17:17 +0100

    Add files via upload

---
+++ Opdracht_016/index.js
@@ -0,0 +1,17 @@
+console.log("Hello Winc Academy")
+let name = "Brigitte";
+console.log(name)
+console.log(4 + 4)
+console.log("4" + "4")
+console.log("4" + 3)
+console.log(3 * 3)
+console.log(200 / 10)
+let x = 5;
+console.log(x + 2);
+let age = 29;
+console.log(age % x)
+console.log(typeof age)
+let agetext = "29";
+console.log(agetext * age)
+console.log(typeof agetext)
+
