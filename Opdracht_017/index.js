28344bde740e2227f8e8e617c52233ae13f1ef4d
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2021-01-28 23:12:14 +0100

    Add files via upload

---
+++ Opdracht_017/index.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+
+//comment
+/*over meerdere
+lijnen nu */
+// control slash is een line comment shortcut
+/* shift alt a is een block comment
+shortcut */
+
+// Dit is een grote som
+/* let multiply = 1230941 * 1823794;
+console.log(multiply);
+let quotient = 637263 / 54;
+console.log(quotient); */
