34d631e85dc7238943a82be4ec70d2116017c9d9
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2021-01-29 12:28:22 +0100

    Add files via upload

---
+++ Opdracht_018/index.js
@@ -0,0 +1,19 @@
+const minAge = 18;
+let yourAge = 18;
+if (yourAge >= minAge) {
+    console.log("Kom binnen schaaaat")
+} else {
+    console.log("Daaaag schaaaat")
+}
+
+const isFemale = false
+if (isFemale) {
+    console.log("Ladiesnight")
+} else { console.log("Hengstenbal") }
+
+const driverStatus = "Bob"
+if ("Bob") {
+    console.log("Hier heb je je autosleutels")
+} else {
+    console.log("Jij gaat niet meer rijden vriendin")
+}
