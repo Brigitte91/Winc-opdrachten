0ffe86abfca82fc6760df8be8c11c14b573b755d
Brigitte91 <71765969+Brigitte91@users.noreply.github.com>
2021-02-17 23:44:49 +0100

    Add files via upload

--- Opdracht_026/index.js
+++ Opdracht_026/index.js
@@ -1,59 +1,60 @@
-let person = prompt("Welkom! Wat is je naam?", "");
-let minNumber = 0
-let maxNumber = 25
+const myGame = function () {
 
+    let person = prompt("Welcome! What's your name?", "");
+    let minNumber = 0
 
-if (person != null) {
-    document.getElementById("name")
-    alert(
-        "Hey " + person + "!");
-    console.log("Name is " + person);
-}
-
-const generateNumber = function (min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(25);
-    return Math.floor(Math.random() * (max - min) + min);
-}
-
-const checkNumber = function (inputNumber) {
-    console.log("Input is " + inputNumber);
-    if (inputNumber <= maxNumber && inputNumber >= minNumber) {
-        return true;
-    } else if (isNaN(inputNumber)) {
-        prompt("Dat is geen getal. Voer een getal in tussen " + minNumber + " en " + maxNumber + " om te beginnen met raden.");
-        return false;
-    } else {
-        prompt("Dat is een te groot of te klein getal. Voer een getal in tussen  " + minNumber + " en " + maxNumber + "  om te beginnen met raden.");
-        return false;
+    if (person != null) {
+        document.getElementById("name")
+        alert(
+            "Hey " + person + "!");
+        console.log("Name is " + person);
     }
-}
 
-const compareNumber = function (inputNumber, generateNumber) {
-    console.log("running the winning machine");
-    if (checkNumber(inputNumber) === true) {
-        if (inputNumber === generateNumber) {
-            alert("You win");
+    let maxNumber = prompt("How high should the highest number be?");
+
+    const generateNumber = function (min, max) {
+        min = Math.ceil(0);
+        max = Math.floor(maxNumber);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    const checkNumber = function (inputNumber) {
+        console.log("Input is " + inputNumber);
+        if (inputNumber <= maxNumber && inputNumber >= minNumber) {
+            return true;
+        } else if (isNaN(inputNumber)) {
+            prompt("Dat is geen getal. Voer een getal in tussen " + minNumber + " en " + maxNumber + " om te beginnen met raden.");
+            return false;
         } else {
-            if (index != 0) {
-                alert("Try again, only " + index + " attempts left");
-            }
-            else { alert("Game over") }
+            prompt("Dat is een te groot of te klein getal. Voer een getal in tussen  " + minNumber + " en " + maxNumber + "  om te beginnen met raden.");
+            return false;
         }
     }
-}
 
-let inputNumber = -1;
-let index = 5;
-let gen_number = generateNumber();
-console.log("Winning number is " + gen_number);
-while (inputNumber != gen_number && index > 0) {
-    index--;
-    console.log(index);
-    inputNumber = parseInt(prompt("Voer een getal in tussen 0 en 25 om te beginnen met raden :)", ""));
-    compareNumber(inputNumber, gen_number);
-}
+    const compareNumber = function (inputNumber, generateNumber) {
+        console.log("running the winning machine");
+        if (checkNumber(inputNumber) === true) {
+            if (inputNumber === generateNumber) {
+                alert("You win");
+            } else {
+                if (index != 0) {
+                    alert("Try again, only " + index + " attempts left");
+                }
+                else { alert(`Game over, the correct number was ${generateNumber}`) }
+            }
+        }
+    }
 
-if (inputNumber != gen_number) {
+    let inputNumber = -1;
+    let index = 5;
+    let gen_number = generateNumber();
     console.log("Winning number is " + gen_number);
+    while (inputNumber != gen_number && index > 0) {
+        index--;
+        console.log(index);
+        inputNumber = parseInt(prompt(`Voer een getal in tussen 0 en ${maxNumber} om te beginnen met raden :)`, ""));
+        compareNumber(inputNumber, gen_number);
+    }
 }
+
+
